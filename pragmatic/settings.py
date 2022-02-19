1e16a7658a24b2193ed7605c7cee2a16f162bcf6
test <rlghks131@korea.ac.kr>
2022-02-19 19:48:50 +0900

    django course 22

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import environ, os
 import os.path
+
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
@@ -138,3 +141,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -15,14 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(),name = 'logout'),
 
     path('account/', include('accountapp.urls'))
 ]
